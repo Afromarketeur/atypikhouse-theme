aa9438f4c9cb4ba00b4f342f947b1596bb87a923
Afromarketeur <kiswen.business@gmail.com>
2025-08-12 19:15:58 +0200

    Create page.php

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <?php while (have_posts()) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <h1><?php the_title(); ?></h1>
+            <div class="content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; ?>
+</div>
+
+<?php get_footer(); ?>
